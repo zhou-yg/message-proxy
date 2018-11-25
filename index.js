8c30a469afa0bb974d1e7fdc9de7f3345babd893
zhouyg <zhou.y.g.890@gmail.com>
2018-11-25 09:25:31 +0800

    fix to es5

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-let argv = process.argv
+var argv = process.argv
 
-let port = +argv[2]
-let name = +argv[3]
+var port = +argv[2]
+var name = +argv[3]
 
 if (port) {
   process.env.PORT = port;
 }
 if (name) {
-  process.env.NAME = name;  
+  process.env.NAME = name;
 }
 
 require('./lib/server');
